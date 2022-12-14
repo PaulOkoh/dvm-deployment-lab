1a55fe33d4e2633614214c057a2b685ae5899b21
Paul Okoh <pauldokoh@gmail.com>
2022-12-14 15:48:03 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(_dirname, '../public/index.html'))
+
+})
+
+app.listen(4004, () =>console.log(`cruising on 4004`))
+
+
+
+
+
